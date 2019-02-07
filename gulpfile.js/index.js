859257ad470958420a8aa964f7ff36a60a3cd2fe
Dustin L. Dodson <Dustinleedodson@gmail.com>
2019-02-06 20:48:53 -0800

    Added linting.

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,39 +3,44 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
+const eslint = require('gulp-eslint');
+
+const lint = () => {
+	return src('src/**/*.js').pipe(eslint({ fix: true }));
+};
 
 const transpile = () => {
-    return browserify('src/app.js')
-        .transform('babelify')
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(dest('docs/js'));
+	return browserify('src/app.js')
+		.transform('babelify')
+		.bundle()
+		.pipe(source('app.js'))
+		.pipe(dest('docs/js'));
 };
 
 const minifyJs = () => {
-    return src('docs/js/app.js')
-        .pipe(uglify())
-        .pipe(dest('docs/js'));
+	return src('docs/js/app.js')
+		.pipe(uglify())
+		.pipe(dest('docs/js'));
 };
 
-const buildJs = series(transpile, minifyJs);
+const buildJs = series(lint, transpile, minifyJs);
 
 const minHtml = () => {
-    return src('src/index.html')
-        .pipe(
-            htmlmin({
-                collapseWhitespace: true,
-                minifyURLs: true,
-                minifyCSS: true,
-                minifyJS: true,
-                removeAttributeQuotes: true,
-                removeComments: true,
-                removeEmptyAttributes: true,
-                removeOptionalTags: true,
-                removeRedundantAttributes: true,
-            })
-        )
-        .pipe(dest('docs/'));
+	return src('src/index.html')
+		.pipe(
+			htmlmin({
+				collapseWhitespace: true,
+				minifyURLs: true,
+				minifyCSS: true,
+				minifyJS: true,
+				removeAttributeQuotes: true,
+				removeComments: true,
+				removeEmptyAttributes: true,
+				removeOptionalTags: true,
+				removeRedundantAttributes: true,
+			})
+		)
+		.pipe(dest('docs/'));
 };
 
 const watchHtml = watch('src/index.html', minHtml);
