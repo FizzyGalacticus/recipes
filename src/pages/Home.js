699ac4a057d2eb7ae0292f62db58d2ee00c1560a
Dustin L. Dodson <dustinleedodson@gmail.com>
2019-04-28 15:36:44 -0700

    Implimented basic home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,19 @@
-export default () => null;
+// @flow
+
+import React, { Component } from 'react';
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Home Page</h1>
+			</div>
+		);
+	}
+}
+
+export default Home;
