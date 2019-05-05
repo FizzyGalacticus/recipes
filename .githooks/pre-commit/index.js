e5943c868053016673097ef861b75db00309ac71
Dustin L. Dodson <dustinleedodson@gmail.com>
2019-05-05 16:26:18 -0700

    Fixed hooks to actually display linting and building process.

--- .githooks/pre-commit/index.js
+++ .githooks/pre-commit/index.js
@@ -1,11 +1,42 @@
 #!/usr/bin/env node
 
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
-const os = require('os');
-const path = require('path');
-const writeFile = promisify(require('fs').writeFile);
+const spawn = require('child_process').spawn;
 const logger = require('node-color-log');
+
+const exec = (cmdStr, { outputToConsole = false } = {}) => {
+	return new Promise((resolve, reject) => {
+		const [cmd, ...args] = cmdStr.split(' ');
+		const proc = spawn(cmd, args, { stdio: outputToConsole ? 'inherit' : 'pipe' });
+
+		let stderr;
+		let stdout;
+
+		if (!outputToConsole) {
+			stderr = '';
+			stdout = '';
+
+			proc.stdout.on('data', data => {
+				stdout += data.toString();
+			});
+
+			proc.stderr.on('data', data => {
+				stderr += data.toString();
+			});
+		}
+
+		proc.on('error', err => {
+			stderr = err;
+		});
+
+		proc.on('exit', code => {
+			if (code !== 0) {
+				reject(stderr);
+			} else {
+				resolve(stdout);
+			}
+		});
+	});
+};
 
 const info = msg => {
 	logger.fontColorLog('blue', msg);
@@ -19,8 +50,12 @@ const error = msg => {
 	logger.fontColorLog('red', msg);
 };
 
+const getJavascriptFiles = (files = []) => {
+	return files.filter(file => file.substring(file.length - 3) === '.js');
+};
+
 const getFilesToBeCommitted = async () => {
-	const { stdout: filesStr } = await exec(`git diff --name-only --cached`);
+	const filesStr = await exec(`git diff --name-only --cached`);
 
 	return filesStr.split('\n');
 };
@@ -31,9 +66,13 @@ const getConfigFilesToBeCommitted = async () => {
 	return files.filter(file => file.includes('config/'));
 };
 
-const writeTempFile = data => {
-	return writeFile(path.join(os.tmpdir(), 'recipesprecommit'), JSON.stringify(data));
+const containsDistFile = (files = []) => {
+	return files.some(file => file.includes('docs/'));
 };
+
+// const writeTempFile = data => {
+// 	return writeFile(path.join(os.tmpdir(), 'recipesprecommit'), JSON.stringify(data));
+// };
 
 const resetConfigFiles = files => {
 	warn(`You have added config files. Removing from commit: (${files.join(', ')})`);
@@ -41,20 +80,29 @@ const resetConfigFiles = files => {
 	return Promise.all(files.map(file => exec(`git reset ${file}`)));
 };
 
-const lint = () => {
+const lint = (files = []) => {
 	info('Linting...');
 
-	return exec(`yarn lint`);
+	const jsFiles = getJavascriptFiles(files);
+
+	return exec(`yarn lint-files --no-ignore ${jsFiles.length ? jsFiles.join(' ') : 'src/**/*.js'}`, {
+		outputToConsole: true,
+	});
 };
 
 const build = () => {
 	info('Building...');
 
-	return exec(`yarn build`);
+	return exec(`yarn build`, { outputToConsole: true });
 };
 
-const addFilesToCommit = files => {
-	return exec(`git add ${files.join(' ')}`);
+const addFilesToCommit = (files = []) => {
+	const filesToAdd = files.filter(file => file !== '');
+	if (!filesToAdd.length) {
+		return Promise.resolve();
+	}
+
+	return exec(`git add ${filesToAdd.join(' ')}`);
 };
 
 (async () => {
@@ -67,8 +115,11 @@ const addFilesToCommit = files => {
 			await resetConfigFiles(configFiles);
 		}
 
-		await lint();
-		await build();
+		await lint(files);
+
+		if (containsDistFile(files)) {
+			await build();
+		}
 
 		await addFilesToCommit(files.filter(file => !file.includes('config/')));
 	} catch (err) {
