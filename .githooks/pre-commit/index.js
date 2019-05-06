74fa02ef93830e3a040df0fd935901f55fd794e4
Dustin L. Dodson <dustinleedodson@gmail.com>
2019-05-05 17:22:28 -0700

    Now prettify files on commit.

--- .githooks/pre-commit/index.js
+++ .githooks/pre-commit/index.js
@@ -5,12 +5,27 @@ const spawn = require('child_process').spawn;
 const logger = require('node-color-log');
 
 type FileList = Array<string>;
+type MessageList = Array<string>;
+
+const info = (...msgs: MessageList) => {
+	logger.fontColorLog('blue', msgs.join(' '));
+};
+
+const warn = (...msgs: MessageList) => {
+	logger.fontColorLog('yellow', msgs.join(' '));
+};
+
+const error = (...msgs: MessageList) => {
+	logger.fontColorLog('red', msgs.join(' '));
+};
 
 const exec = (
 	cmdStr: string,
 	{ outputToConsole = false }: { outputToConsole?: boolean } = {}
 ): Promise<string | undefined> => {
 	return new Promise((resolve, reject) => {
+		info(`Running cmd: "${cmdStr}"`);
+
 		const [cmd, ...args] = cmdStr.split(' ');
 		const proc = spawn(cmd, args, { stdio: outputToConsole ? 'inherit' : 'pipe' });
 
@@ -44,18 +59,6 @@ const exec = (
 	});
 };
 
-const info = (msg: string) => {
-	logger.fontColorLog('blue', msg);
-};
-
-const warn = (msg: string) => {
-	logger.fontColorLog('yellow', msg);
-};
-
-const error = (msg: string) => {
-	logger.fontColorLog('red', msg);
-};
-
 const getJavascriptFiles = (files: FileList = []) => {
 	return files.filter(file => file.substring(file.length - 3) === '.js');
 };
@@ -66,9 +69,7 @@ const getFilesToBeCommitted = async () => {
 	return filesStr.split('\n');
 };
 
-const getConfigFilesToBeCommitted = async () => {
-	const files = await getFilesToBeCommitted();
-
+const getConfigFiles = (files: FileList = []) => {
 	return files.filter(file => file.includes('config/'));
 };
 
@@ -80,6 +81,14 @@ const resetConfigFiles = (files: FileList = []) => {
 	warn(`You have added config files. Removing from commit: (${files.join(', ')})`);
 
 	return Promise.all(files.map(file => exec(`git reset ${file}`)));
+};
+
+const prettier = (files: FileList = []) => {
+	info('Prettifying...');
+
+	const jsFiles = getJavascriptFiles(files);
+
+	return exec(`yarn prettier-files ${jsFiles.length ? jsFiles.join(' ') : 'src/**/*.js'}`);
 };
 
 const lint = (files: FileList = []) => {
@@ -110,7 +119,7 @@ const addFilesToCommit = (files: FileList = []) => {
 (async () => {
 	try {
 		const files = await getFilesToBeCommitted();
-		const configFiles = await getConfigFilesToBeCommitted();
+		const configFiles = getConfigFiles(files);
 
 		// Do not commit config files without explictly not running hooks
 		if (configFiles.length) {
@@ -118,6 +127,7 @@ const addFilesToCommit = (files: FileList = []) => {
 		}
 
 		await lint(files);
+		await prettier(files);
 
 		if (containsDistFile(files)) {
 			await build();
